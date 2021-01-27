59322f355d37cc43346072c163097f2b47788097
Imangali <Imangali@gmail.com>
2021-01-27 20:35:50 +0600

    VERY basic image upload functionality

--- backend/server/ml/models.py
+++ backend/server/ml/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    """ This model keeps all images of users before enhancing """  
+    
+    name      = models.CharField(max_length=50) 
+    image     = models.ImageField(upload_to='images/')
+
+ 
--- backend/server/ml/views.py
+++ backend/server/ml/views.py
@@ -1,2 +1,25 @@
 from django.shortcuts import render
 
+from ml.ml_service import ML
+
+
+from django.http import HttpResponse 
+from django.shortcuts import render, redirect 
+from .forms import *
+  
+# Create your views here. 
+def image_view(request): 
+  
+    if request.method == 'POST': 
+        form = HotelForm(request.POST, request.FILES) 
+  
+        if form.is_valid(): 
+            form.save() 
+            return redirect('success') 
+    else: 
+        form = HotelForm() 
+    return render(request, 'image_upload.html', {'form' : form}) 
+  
+  
+def success(request): 
+    return HttpResponse('successfully uploaded') 
